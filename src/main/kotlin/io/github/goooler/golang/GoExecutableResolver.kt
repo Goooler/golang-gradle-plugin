f6d2ab0d67a57840596cc8adf35ea5ed904393a4
Goooler <wangzongler@gmail.com>
2026-02-23 18:06:35 +0800

    Cleanup

--- src/main/kotlin/io/github/goooler/golang/GoExecutableResolver.kt
+++ src/main/kotlin/io/github/goooler/golang/GoExecutableResolver.kt
@@ -18,10 +18,12 @@ internal fun resolveGoExecutable(providerFactory: ProviderFactory): Provider<Str
     when (os) {
       OS.MACOS -> {
         candidates.add(File("/usr/local/go/bin/$goName"))
-        candidates.add(File("/opt/homebrew/bin/$goName"))
+        candidates.add(File("/opt/homebrew/bin/$goName")) // Homebrew on Apple Silicon.
+        candidates.add(File("/usr/local/bin/$goName")) // Homebrew on Intel.
       }
       OS.LINUX -> {
         candidates.add(File("/usr/local/go/bin/$goName"))
+        candidates.add(File("/usr/local/bin/$goName"))
         candidates.add(File("/usr/bin/$goName"))
         candidates.add(File("/snap/bin/$goName"))
         candidates.add(File("/home/linuxbrew/.linuxbrew/bin/$goName"))
@@ -30,9 +32,6 @@ internal fun resolveGoExecutable(providerFactory: ProviderFactory): Provider<Str
         candidates.add(File("C:\\Program Files\\Go\\bin\\$goName"))
         candidates.add(File("C:\\Go\\bin\\$goName"))
       }
-    }
-    if (os != OS.WINDOWS) {
-      candidates.add(File("/usr/local/bin/$goName"))
     }
 
     candidates.firstOrNull { it.exists() && it.isFile && it.canExecute() }?.absolutePath ?: "go"
